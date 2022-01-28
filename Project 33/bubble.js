3c75fac74724db4774eb0caa8039e911d9d08d9d
MIGK25 <87221499+MIGK25@users.noreply.github.com>
2022-01-28 20:58:34 +0530

    Add files via upload

---
+++ Project 33/bubble.js
@@ -0,0 +1,25 @@
+class Bubble 
+{
+  constructor(x, y, w,h) 
+  {
+    let options = {
+     isStatic:true
+    };
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
+  }
+
+  show() {
+    var pos = this.body.position;
+    var angle = this.body.angle
+    push();
+    rotate(angle);
+    fill("blue");
+    ellipseMode(CENTER);
+    ellipse(0, 0, this.w, this.h);
+    pop();
+  }
+}
